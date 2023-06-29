568278a2cfd809f344e644979d42f3c6aff3e5db
hanleymark <home@markhanley.co.uk>
2023-06-29 16:26:52 +0100

    Test pre-commit format

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <h1>Hello Moodsters!</h1>;
+  return <h1>Welcome Moodsters!</h1>;
 }
