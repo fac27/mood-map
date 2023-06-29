01e01ea751e1df3476554a20a5f81c9af943057b
FomasTreeman <tom@team-freeman.com>
2023-06-29 16:10:22 +0100

    initial

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import {Link} from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <h1>Hello Moodsters!</h1>;
+  return <>
+    <h1>Hello Moodsters!</h1>;
+    <Link to="login" />
+  </>
 }
---
+++ src/pages/login.js
@@ -0,0 +1,7 @@
+export default function login() {
+    return (
+        <form>
+            <input> name </input>
+        </form>
+    )
+}
