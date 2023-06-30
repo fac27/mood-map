a1f9fde60ef92c1eb8b47f3d036ad368805f3851
FomasTreeman <tom@team-freeman.com>
2023-06-30 10:58:51 +0100

    initial

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,5 +2,12 @@ import Image from "next/image";
 import image from "../../images/Untitled.jpg";
 
 export default function login() {
-  return <Image src={image} alt="gif" width={100} height={100} />;
+  return (
+    <>
+      <form>
+        <input> email </input>
+      </form>
+      <Image src={image} alt="gif" width={100} height={100} />;
+    </>
+  );
 }
