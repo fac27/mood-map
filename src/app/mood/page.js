dbbcb2636eadb4b7424a3d744b58ab6bd9311e00
FomasTreeman <tom@team-freeman.com>
2023-06-30 11:07:58 +0100

    images

--- src/app/mood/page.js
+++ src/app/mood/page.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import Image from "next/image";
 import image from "../../images/Untitled.jpg";
 
 export default function mood() {
+  //   const [emotion, setEmotion] = useState("emo1");
+
   return (
     <>
-      <Image src={image} alt="image" width={500} height={500} />
+      <Image src={emo1} alt="image" width={500} height={500} />
+      <Image src={emo1} alt="image" width={500} height={500} />
+      <Image src={emo2} alt="image" width={500} height={500} />
+      <Image src={emo3} alt="image" width={500} height={500} />
+      <Image src={emo4} alt="image" width={500} height={500} />
+      <Image src={emo5} alt="image" width={500} height={500} />
     </>
   );
 }
