b0ad3cb93c7bcaa998c77de264f97ab176b69e5c
FomasTreeman <tom@team-freeman.com>
2023-07-03 16:39:06 +0100

    checkuser

--- src/lib/supabaseSession.js
+++ src/lib/supabaseSession.js
@@ -1,18 +1,21 @@
 import { redirect } from "next/navigation";
 import { supabaseBrowser, supabaseServer } from "./supabaseClient";
 
+function checkUser(session) {
+  if (!session?.access_token) return redirect("/login");
+  return session.user;
+}
+
 export async function getSessionServer() {
   const {
     data: { session },
   } = await supabaseServer().auth.getSession();
-  if (!session?.access_token) return redirect("/login");
-  return session.user;
+  return checkUser(session);
 }
 
 export async function getSessionBrowser() {
   const {
     data: { session },
   } = await supabaseBrowser.auth.getSession();
-  if (!session?.access_token) return redirect("/login");
-  return session.user;
+  return checkUser(session);
 }
