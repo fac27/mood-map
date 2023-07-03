896b69c4f4f611f91fe6aab928ee26d6acd1735d
hanleymark <home@markhanley.co.uk>
2023-07-03 16:20:19 +0100

    Add @ to next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.join(__dirname, 'src');
+    
+        return config;
+      },
+};
 
 module.exports = nextConfig;
