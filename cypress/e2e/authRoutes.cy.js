06a6f8dbed1ae53c971d1f46de353f01fc8c0658
FomasTreeman <tom@team-freeman.com>
2023-07-03 17:44:48 +0100

    tests

--- cypress/e2e/authRoutes.cy.js
+++ cypress/e2e/authRoutes.cy.js
@@ -1,13 +1,21 @@
 const paths = ["/", "/detail", "/life-in-color", "/mood"];
+const url = "https://mood-map.vercel.app";
 
 paths.forEach((path) => {
-  describe("Test every route redirects to login if no valid session", () => {
+  describe(`Test ${path} route redirects to login if no valid session`, () => {
     it("should redirect to login if no session", () => {
-      cy.visit(path);
-      cy.url().should("eq", "/login");
+      cy.visit(url + path);
+      cy.url().should("eq", url + "/login"); // check redirect
     });
     it("should not redirect but should instead show email", () => {
-      cy.visit("/login");
+      cy.visit(url + "/mood");
+      cy.get("#email").type("tom@team-freeman.com");
+      cy.get("#password").type("password");
+      cy.get("#auth-sign-in").click();
+      cy.visit(url + path);
+      cy.url()
+        .should("eq", url + "/login")
+        .not(); // check redirect
     });
   });
 });
