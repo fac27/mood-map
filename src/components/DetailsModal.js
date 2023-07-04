2ecb3cdd29b1f653f2093d8bc48a82b171601850
FomasTreeman <tom@team-freeman.com>
2023-07-04 16:34:37 +0100

    final revision

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -3,7 +3,6 @@ import { useRef, useState } from "react";
 import styles from "./DetailsModal.module.css";
 import Image from "next/image";
 import Link from "next/link";
-// import { redirect } from "next/navigation";
 
 const today = new Date();
 const pad = (num) => num.toString().padStart(2, "0");
@@ -24,8 +23,6 @@ const formHeadings = [
   { name: "context_location", heading: "Where were you?", inputType: "text" },
 ];
 
-console.log(`FS: ${JSON.stringify(initialFormState)}`);
-
 export default function DetailsModal({ emotion }) {
   const [mood, setMood] = useState(initialFormState);
   const link = useRef();
@@ -42,7 +39,6 @@ export default function DetailsModal({ emotion }) {
     const user = session.user;
     mood.user_id = user.id;
     mood.mood = emotion;
-    console.log(mood);
     const { error } = await supabaseBrowser.from("entries").insert(mood);
     console.log(`ERROR: ${JSON.stringify(error)}`);
     if (error === null) {
@@ -74,37 +70,6 @@ export default function DetailsModal({ emotion }) {
             />
           </>
         ))}
-        {/* <label htmlFor="mood_date">Date</label>
-      <input
-        type="date"
-        name="mood_date"
-        id="mood_date"
-        value={mood.mood_date}
-        onChange={handleChange}
-      />
-      <label htmlFor="journal_entry">Journal Entry</label>
-      <textarea
-        name="journal_entry"
-        id="journal_entry"
-        value={mood.journal_entry}
-        onChange={handleChange}
-      />
-      <label htmlFor="context_people">Who were you with?</label>
-      <input
-        type="text"
-        name="context_people"
-        id="context_people"
-        value={mood.context_people}
-        onChange={handleChange}
-      />
-      <label htmlFor="context_location">Where were you?</label>
-      <input
-        type="text"
-        name="context_location"
-        id="context_location"
-        value={mood.context_location}
-        onChange={handleChange}
-      /> */}
         <button type="submit">Submit</button>
       </form>
       <Link href={"/life-in-colour"} ref={link} />
