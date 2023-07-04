03197cf3f195038d00299b82b8fe8940d235c1b5
cameochoquer <cjchoquer@gmail.com>
2023-07-04 16:01:20 +0100

    fix: husky

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,8 +1,8 @@
-describe('Navigation', ()=>{
- it('should navigate to the life-in-colour page', ()=>{
-    cy.visit('/life-in-colour')
-    cy.get('Link[href*="life-in-colour"]').click()
-    cy.url().should('include', '/life-in-colour')
-    cy.get('h1').contains('Life in Colour')
- })
-})
+describe("Navigation", () => {
+  it("should navigate to the life-in-colour page", () => {
+    cy.visit("/life-in-colour");
+    cy.get('Link[href*="life-in-colour"]').click();
+    cy.url().should("include", "/life-in-colour");
+    cy.get("h1").contains("Life in Colour");
+  });
+});
