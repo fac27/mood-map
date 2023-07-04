1058182e37c93792055fb704cd7a25bbb9cd9a50
Zakaria Yaris <info@zakariya.me>
2023-07-04 16:14:47 +0100

    life in colour page done

--- src/app/life-in-colour/page.tsx
+++ src/app/life-in-colour/page.tsx
@@ -1,39 +1,28 @@
 import { ReactElement, FC } from "react";
 import styles from "./page.module.css";
-import { getDays } from "../../utils/getDays";
+import { getMonths } from "../../utils/dateHelpers";
 
 const Grid: FC = (): ReactElement => {
-  const divDays = getDays(2023);
-  const divDayElements: ReactElement[] = divDays.map((day: Date) => {
-    const dateOfMonth = day.getDate();
-    const firstDayOfWeek = new Date(
-      day.getFullYear(),
-      day.getMonth(),
-      1
-    ).getDay();
-    let gridColumn = ((firstDayOfWeek + 6) % 7) + 1;
-
-    if (dateOfMonth > 1) {
-      gridColumn = ((gridColumn + dateOfMonth - 2) % 7) + 1;
+  const monthData: { [key: string]: Date[] } = getMonths(2023);
+  const dateElements: ReactElement[] = Object.entries(monthData).map(
+    ([month, days], idx) => {
+      return (
+        <div key={idx} className={styles.monthContainer}>
+          <h4>{month}</h4>
+          <ul className={styles.dayContainer}>
+            {days.map((day, idx) => (
+              <li key={idx} className={styles.dayBox}></li>
+            ))}
+          </ul>
+        </div>
+      );
     }
-    return <div className={styles.gridBox} key={day.toString()}></div>;
-  });
+  );
 
   return (
     <>
       <h1 className={styles.header}>My Life in Colour</h1>
-      <div className={styles.container}>
-        <div className={styles.gridContainer}>
-          <p>M</p>
-          <p>T</p>
-          <p>W</p>
-          <p>T</p>
-          <p>F</p>
-          <p>S</p>
-          <p>S</p>
-        </div>
-        <div className={styles.gridBoxes}>{divDayElements}</div>
-      </div>
+      <div className={styles.gridContainer}>{dateElements}</div>
     </>
   );
 };
---
+++ src/utils/dateHelpers.ts
@@ -0,0 +1,25 @@
+export const getMonths = (year: number): { [key: string]: Date[] } => {
+  const months: { [key: string]: Date[] } = {};
+  const startDate = new Date(year, 0, 1);
+
+  for (let month = 0; month < 12; month++) {
+    const endDate = new Date(year, month + 1, 0);
+    const currentMonth = startDate.toLocaleString("default", { month: "long" });
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= endDate) {
+      if (!months[currentMonth]) {
+        months[currentMonth] = [];
+      }
+
+      const dateWithoutTime = new Date(currentDate);
+      dateWithoutTime.setHours(0, 0, 0, 0);
+      months[currentMonth].push(dateWithoutTime);
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    startDate.setMonth(startDate.getMonth() + 1);
+  }
+
+  return months;
+};
