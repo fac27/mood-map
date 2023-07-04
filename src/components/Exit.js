6b2288c18c08d400192752c9373ee887056686cb
cameochoquer <cjchoquer@gmail.com>
2023-07-04 11:37:34 +0100

    fix: path in exit.js

--- src/components/Exit.js
+++ src/components/Exit.js
@@ -3,7 +3,7 @@ import styles from "./Exit.module.css";
 export default function Exit({ path }) {
   return (
     <span className={styles.float}>
-      <Link href={"/life-in-colour"} style={{ textDecoration: "none" }}>
+      <Link href={path} style={{ textDecoration: "none" }}>
         ❌
       </Link>
     </span>
