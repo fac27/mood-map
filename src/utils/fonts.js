5785ed7ced47edc696a49ee440e3f82f3042dc5c
FomasTreeman <tom@team-freeman.com>
2023-07-04 10:42:08 +0100

    working version

--- src/utils/fonts.js
+++ src/utils/fonts.js
@@ -3,9 +3,11 @@ import { Yeseva_One, Josefin_Sans } from "next/font/google";
 export const yesevaOne = Yeseva_One({
   weight: "400",
   subsets: ["latin"],
+  variable: "--font-header",
 });
 
 export const josefinSans = Josefin_Sans({
   weight: "300",
   subsets: ["latin"],
+  variable: "--font-content",
 });
