6d7f0f92246d960490f313c50ef5748c7ea978cd
FomasTreeman <tom@team-freeman.com>
2023-07-04 11:31:53 +0100

    removed unnecesary joesfin snas import

--- src/app/mood/page.js
+++ src/app/mood/page.js
@@ -3,7 +3,6 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { josefinSans } from "@/utils/fonts";
 import Exit from "../../components/Exit";
 import emo1 from "../../../public/images/emo1.png";
 import emo2 from "../../../public/images/emo2.png";
