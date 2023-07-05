3e6f9b4ab675e5f6a4111e6a294820c7cc9673c7
FomasTreeman <tom@team-freeman.com>
2023-07-05 14:19:45 +0100

    mood page hotfix

--- src/app/mood/page.js
+++ src/app/mood/page.js
@@ -1,27 +1,27 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Exit from "@/components/Exit";
-import emo1 from "../../../public/images/emo1.svg";
-import emo2 from "../../../public/images/emo2.svg";
-import emo3 from "../../../public/images/emo3.svg";
-import emo4 from "../../../public/images/emo4.svg";
-import emo5 from "../../../public/images/emo5.svg";
+// import emo1 from "../../../public/images/emo1.svg";
+// import emo2 from "../../../public/images/emo2.svg";
+// import emo3 from "../../../public/images/emo3.svg";
+// import emo4 from "../../../public/images/emo4.svg";
+// import emo5 from "../../../public/images/emo5.svg";
 import styles from "./page.module.css";
 import DetailsModal from "@/components/DetailsModal";
 
 export default function MoodPicker() {
-  const [emotion, setEmotion] = useState(emo4);
+  const [emotion, setEmotion] = useState(4);
   const [showDetails, setShowDetails] = useState(false);
 
-  const emojiElements = [emo1, emo2, emo3, emo4, emo5].map((svg, i) => (
+  const EmojiElements = [1, 2, 3, 4, 5].map((emotion) => (
     <Image
-      key={i}
+      key={emotion}
       className={styles.emojiBox}
-      onClick={() => setEmotion(svg)}
-      src={svg}
+      onClick={() => setEmotion(emotion + 1)}
+      src={`/images/emo${emotion}.svg`}
       alt="image"
       width={60}
       height={60}
@@ -35,12 +35,14 @@ export default function MoodPicker() {
         <h1 className={styles.title}>How are you feeling today?</h1>
         <Image
           className={styles.selectedImage}
-          src={emotion}
+          src={`/images/emo${emotion}.svg`}
           alt="defualt emotion"
         />
       </div>
 
-      <div className={styles.emojiContainer}>{emojiElements}</div>
+      <div className={styles.emojiContainer}>
+        <EmojiElements />
+      </div>
 
       <div className={styles.links}>
         <button onClick={() => setShowDetails(true)}>Give more detail?</button>
