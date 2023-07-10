40e31caa89c528aa920d517f1e2f06ca61bd9e5c
cameochoquer <cjchoquer@gmail.com>
2023-07-10 15:38:59 +0100

    husky

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import nextJest from "next/jest";
 const createJestConfig = nextJest({ dir: "./" });
 
 const customJestConfig = {
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  setupFilesAfterEnv: ["./jest.setup.js"],
   testEnvironment: "jsdom",
   verbose: true,
   collectCoverage: true,
@@ -13,11 +13,11 @@ const customJestConfig = {
     "src/lib/server/client.js",
   ],
   moduleNameMapper: {
-    '^jose$': require.resolve('jose'),
+    "^jose$": require.resolve("jose"),
   },
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
   coverageDirectory: "coverage",
- 
+
   transform: {
     "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": "ts-jest",
   },
--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
 import "@testing-library/jest-dom";
-const util = require('util');
+const util = require("util");
 global.TextEncoder = util.TextEncoder;
 global.TextDecoder = util.TextDecoder;
