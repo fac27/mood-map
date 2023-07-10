06d820d81baf86a332d858242cb3542f53233158
cameochoquer <cjchoquer@gmail.com>
2023-07-10 17:42:39 +0100

    husky fix

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,10 +1,10 @@
 const date = () => {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
-    const day = String(currentDate.getDate()).padStart(2, "0");
-    
-    return `${year}-${month}-${day}`;
-}
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
 
 export default date;
