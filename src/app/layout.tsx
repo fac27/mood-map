538c27272dd290fcf8c54d5633962baad3375e17
cameochoquer <cjchoquer@gmail.com>
2023-07-11 15:31:43 +0100

    logout conditional

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,21 @@ import "./globals.css";
 import { yesevaOne } from "../utils/fonts";
 import Logout from "@/components/Logout";
 import { ReactNode } from "react";
-
+import { getSessionServer } from "@/lib/server/session";
 export const metadata: Record<string, any> = {};
 
 interface RootLayoutProps {
   children: ReactNode;
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default async function RootLayout({ children }: RootLayoutProps) {
+  const session = await getSessionServer();
+
   return (
     <>
       <html lang="en">
         <body className={yesevaOne.className}>
-          <Logout />
+          {session ? <Logout /> : ""}
           {children}
         </body>
       </html>
