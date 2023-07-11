112caa87d511521cd89345d04cdaaa5da8ecc22f
FomasTreeman <tom@team-freeman.com>
2023-07-11 18:04:13 +0100

    merge ts error

--- src/app/mood/page.tsx
+++ src/app/mood/page.tsx
@@ -3,6 +3,6 @@ import MoodPicker from "./components/MoodPicker";
 import { Session } from "@supabase/auth-helpers-nextjs";
 
 export default async function Mood() {
-  const session = (await getSessionServer()) as Session;
+  const session = await getSessionServer() as Session;
   return <MoodPicker session={session} />;
 }
