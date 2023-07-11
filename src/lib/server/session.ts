a1939c294628e795417181a19a925b1fbf9ee85f
hanleymark <home@markhanley.co.uk>
2023-07-11 12:40:51 +0100

    ts refactor of lib folder complete

--- src/lib/server/session.ts
+++ src/lib/server/session.ts
@@ -1,14 +1,15 @@
+import { Session } from "@supabase/auth-helpers-nextjs";
 import supabaseServer from "./client";
 import { redirect } from "next/navigation";
 
-export async function getSessionServer() {
+export async function getSessionServer(): Promise<Session | null> {
   const {
     data: { session },
   } = await supabaseServer().auth.getSession();
   return session;
 }
 
-export async function protectServerRoute() {
+export async function protectServerRoute(): Promise<Session | null> {
   const session = await getSessionServer();
   if (!session?.access_token) return redirect("/login");
   return session;
