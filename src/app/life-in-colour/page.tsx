9e35c6d55da8d7111e647750ea2bf281645f0b1a
FomasTreeman <tom@team-freeman.com>
2023-07-11 17:12:29 +0100

    wrokgin

--- src/app/life-in-colour/page.tsx
+++ src/app/life-in-colour/page.tsx
@@ -2,8 +2,21 @@ import { ReactElement, FC } from "react";
 import styles from "./page.module.css";
 import Navbar from "@/components/Navbar";
 import GridDays from "./components/GridDays";
+import { getSessionServer } from "@/lib/server/session";
+import { redirect } from "next/navigation";
+import { getAllEntries } from "@/lib/models";
 
-const Grid: FC = (): ReactElement => {
+const Grid: FC = async (): Promise<ReactElement> => {
+    const session = await getSessionServer();
+    if (!session) redirect('/login')
+    
+    const allEntries = await getAllEntries(session.user.id)
+    const entriesSortedByDate = allEntries.sort((a, b) => {
+      const dateA = new Date(a.mood_date);
+      const dateB = new Date(b.mood_date);
+      return dateA < dateB ? -1 : dateA > dateB ? 1 : 0;
+    });
+
   return (
     <>
       <h1 className={styles.pageHeader}>My Life in Colour</h1>
@@ -18,7 +31,7 @@ const Grid: FC = (): ReactElement => {
           <p>S</p>
         </div>
         <div className={styles.grid}>
-          <GridDays />
+          <GridDays allEntries={entriesSortedByDate}/>
         </div>
       </div>
       <Navbar />
