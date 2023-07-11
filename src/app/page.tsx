3c94df142c0cd8bdd38933edf3914e5ba41e7abc
cameochoquer <cjchoquer@gmail.com>
2023-07-11 12:43:53 +0100

    husky fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,61 +16,60 @@ async function checkEntryForToday(userId: string): Promise<IEntry> {
   return entry;
 }
 
-export default async function Home():Promise<ReactElement> {
-    const session = await protectServerRoute();
-    const user = session.user;
-    const entry: IEntry = await checkEntryForToday(user.id);
-    const entryInfo = Object.values(entry).slice(2);
-  
-    const blobElements = entryInfo.map((info, idx) => {
-      const svg = generateBlob();
-  
-      return (
-        <div key={uuidv4()} className={styles.blob}>
-          <div className={`${styles.textContainer} ${josefinSans.className}`}>
-            {formatText(info).map((line) => (
-              <p
-                className={
-                  svg.colour === "light"
-                    ? `${styles.darkText}`
-                    : `${styles.lightText}`
-                }
-                key={uuidv4()}
-              >
-                {line.join("")}
-              </p>
-            ))}
-          </div>
-  
-          <svg viewBox={svg.viewBox} xmlns={svg.xmlns}>
-            <path
-              fill={svg.path.fill}
-              d={svg.path.d}
-              transform={svg.path.transform}
-            />
-          </svg>
-        </div>
-      );
-    });
-  
+export default async function Home(): Promise<ReactElement> {
+  const session = await protectServerRoute();
+  const user = session.user;
+  const entry: IEntry = await checkEntryForToday(user.id);
+  const entryInfo = Object.values(entry).slice(2);
+
+  const blobElements = entryInfo.map((info, idx) => {
+    const svg = generateBlob();
+
     return (
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1>Hello, {user.email} </h1>
-          <p className={styles.moodHeader}>
-            Mood for the day{" "}
-            <Image
-              src={`/images/emo${entry.mood}.svg`}
-              alt="mood of the day"
-              width={60}
-              height={60}
-            />
-          </p>
+      <div key={uuidv4()} className={styles.blob}>
+        <div className={`${styles.textContainer} ${josefinSans.className}`}>
+          {formatText(info).map((line) => (
+            <p
+              className={
+                svg.colour === "light"
+                  ? `${styles.darkText}`
+                  : `${styles.lightText}`
+              }
+              key={uuidv4()}
+            >
+              {line.join("")}
+            </p>
+          ))}
         </div>
-  
-        <div className={styles.blobContainer}>{blobElements}</div>
-        <Navbar />
+
+        <svg viewBox={svg.viewBox} xmlns={svg.xmlns}>
+          <path
+            fill={svg.path.fill}
+            d={svg.path.d}
+            transform={svg.path.transform}
+          />
+        </svg>
       </div>
     );
-  }
-  
+  });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>Hello, {user.email} </h1>
+        <p className={styles.moodHeader}>
+          Mood for the day{" "}
+          <Image
+            src={`/images/emo${entry.mood}.svg`}
+            alt="mood of the day"
+            width={60}
+            height={60}
+          />
+        </p>
+      </div>
+
+      <div className={styles.blobContainer}>{blobElements}</div>
+      <Navbar />
+    </div>
+  );
+}
