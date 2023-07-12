d08d76f608cee3fe1ea201ccd20c8308dda80d9a
Zakaria Yaris <info@zakariya.me>
2023-07-12 12:30:12 +0100

    add any to data in LoginForm component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,8 @@
 import { getSessionServer } from "@/lib/server/session";
 import { Session } from "@supabase/auth-helpers-nextjs";
-import LoginComp from "./components/LoginForm";
+import LoginForm from "./components/LoginForm";
 
 export default async function Login() {
   const session = (await getSessionServer()) as Session;
-  return <LoginComp session={session} />;
+  return <LoginForm session={session} />;
 }
