7056b0a122e0a808b8b0b292eceb8fddc1f5faa9
cameochoquer <cjchoquer@gmail.com>
2023-07-13 10:29:33 +0100

    fix: husky

--- src/utils/spotifyHelper.ts
+++ src/utils/spotifyHelper.ts
@@ -23,18 +23,20 @@ export default async function getRecentlyPlayedSong(
     );
     if (!response.ok) {
       if (response.status === 504) {
-        console.error("Service didn't reply before timeout. Please try again later.");
+        console.error(
+          "Service didn't reply before timeout. Please try again later."
+        );
       } else {
-        throw new Error('HTTP error ' + response.status);
+        throw new Error("HTTP error " + response.status);
       }
     } else {
-    const responseJson = await response.json();
-    console.log('😡',responseJson)
-    // const tracks = responseJson.items.track.href;
-    const trackHrefs = Array.isArray(responseJson.items)
-    ? responseJson.items.map((item: any) => item.track.href)
-    : [];
-    return trackHrefs[trackHrefs.length - 1];
+      const responseJson = await response.json();
+      console.log("😡", responseJson);
+      // const tracks = responseJson.items.track.href;
+      const trackHrefs = Array.isArray(responseJson.items)
+        ? responseJson.items.map((item: any) => item.track.href)
+        : [];
+      return trackHrefs[trackHrefs.length - 1];
     }
   } catch (e) {
     console.error("❌", e);
